e63e403dee76a69f11784f820dc71c3f6f3d08eb
Chim Sovann <85720970+sovann-maker@users.noreply.github.com>
2022-02-16 20:10:16 +0700

    first commit

--- models/movie.js
+++ models/movie.js
@@ -1,10 +1,10 @@
 class Movie {
-    constructor(mid, mname, mrating, mindustry, mdate ) {
-            this.mid = mid;
-            this.mname = mname;
-            this.mrating = mrating;
-            this.mindustry =  mindustry;
-            this.mdate = mdate;
+    constructor(mid, mname, mrating, mindustry, mdate) {
+        this.mId = mId;
+        this.mName = mName;
+        this.mRating = mRating;
+        this.mIndustry = mIndustry;
+        this.mDate = mDate;
     }
 }
 
